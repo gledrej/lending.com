0412504f772e01ef07370f9c9294e38fd5ecd6d4
gledrej <gledrej1988@gmail.com>
2016-07-10 23:38:02 +0300

    add

--- index.php
+++ index.php
@@ -13,18 +13,49 @@
 </head>
 <body>
 
-<div class="container-fluid" style="background-color: #0f0f0f">
-    <div class="container" style="background-color: #f02517">
+<div class="container-fluid header_top_bg">
+    <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-md-2 col-sm-6 col-xs-12" style="background-color: #ff964f">test</div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="background-color: #5d33f3">test</div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="background-color: #43ee76">test</div>
-            <div class="col-lg-3 col-md-2 col-sm-6 col-xs-12" style="background-color: #fdff1d">test</div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 pull-left">
+                <div class="header_tel-icon col-lg-3 col-md-3 col-sm-3 offset-xs ">
+                    <img class="header_images_tel" src="images/tel.png">
+                </div>
+                <div class="header_tel-number col-lg-9 col-md-9 col-sm-12 col-xs-12 text-left ">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">+38(094) 94 750 77</div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">+38(094) 94 750 77</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 pull-right header_soc_set">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/facebook.png">
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/twitter.png">
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/instagram.png">
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <a href="#">
+                        <img src="images/facebook.png">
+                    </a>
+                </div>
+                
+            </div>
         </div>
     </div>
 </div>
 
-<div>p</div>
+
 
 <div class="container-fluid" style="background-color: #0f0f0f">
     <div class="row">
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 /**
  * Created by Artem on 10.07.2016.
  */
+   
