75c7e813608c857cbde3701875c7e3d1133e64f2
Nassipa <anastasiapashina7696@gmail.com>
2016-07-11 01:04:48 +0300

    add

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <meta name="description" content="test">
     <meta name="keywords" content="test">
-
+    <meta charset="utf-8">
 
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/bootstrap.css">
@@ -197,7 +197,73 @@
 
 
 
-/* Cod Nasti*/
+<!--Cod Nasti-->
+<div class="container-fluid block_video_bg" >
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 text-center">
+                <h1>- - - Почему Надувной Банан так нравится покупателям? - - -</h1>
+            </div>
+            <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+
+                <div class="col-lg-6 col-md-6 col-xs-6 col-sm-6 block_video">
+                    <iframe width="560" height="330" src="https://www.youtube.com/embed/8yqrF5fMzok" frameborder="0"></iframe>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-xs-6 col-sm-6 block_arrow_text">
+                    <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+
+                        <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                        <div class="col-lg-2 col-md-2 col-xs-2 col-sm-2 text-center">
+                            <img src="images/arrow_black.png">
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-xs-10 col-sm-10">
+                            <p>Форма дивана подстраивается под вашу фигуру,<br> выдерживается вес до 250 кг</p>
+                        </div>
+                        </div>
+
+                        <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                        <div class="col-lg-2 col-md-2 col-xs-2 col-sm-2 text-center">
+                            <img src="images/arrow_black.png">
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-xs-10 col-sm-10">
+                            <p>В собранном состоянии весит чуть более килограмма и имеер размер 20 на 35</p>
+                        </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                        <div class="col-lg-2 col-md-2 col-xs-2 col-sm-2 text-center">
+                            <img src="images/arrow_black.png">
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-xs-10 col-sm-10">
+                            <p>Диван создан из прочной водонепронецаемой ткани, крайне устойчив к износу и повреждениям</p>
+                        </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                        <div class="col-lg-2 col-md-2 col-xs-2 col-sm-2 text-center">
+                            <img src="images/arrow_black.png">
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-xs-10 col-sm-10">
+                            <p>Диван-Банан надувается за 15 секунд без насоса и другого специального оборудования</p>
+                        </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                        <div class="col-lg-2 col-md-2 col-xs-2 col-sm-2 text-center">
+                            <img src="images/arrow_black.png">
+                        </div>
+                        <div class="col-lg-10 col-md-10 col-xs-10 col-sm-10">
+                            <p>В Вашем распоряжении коллекция моделей различных цветов. Вибирайте диван любого</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
 
 
 </body>
