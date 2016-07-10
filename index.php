03f114bd7f78cb97eb3db1aac5d4940761728955
gledrej <gledrej1988@gmail.com>
2016-07-10 20:12:05 +0300

    sait

---
+++ index.php
@@ -0,0 +1,41 @@
+<html lang="ua">
+<head>
+    <meta name="description" content="test">
+    <meta name="keywords" content="test">
+
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script type="jscript" src="js/bootstrap.js"></script>
+</head>
+<body>
+
+<div class="container-fluid" style="background-color: #0f0f0f">
+    <div class="container" style="background-color: #f02517">
+        <div class="row">
+            <div class="col-lg-3 col-md-2 col-sm-6 col-xs-12" style="background-color: #ff964f">test</div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="background-color: #5d33f3">test</div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="background-color: #43ee76">test</div>
+            <div class="col-lg-3 col-md-2 col-sm-6 col-xs-12" style="background-color: #fdff1d">test</div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid" style="background-color: #0f0f0f">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background-color: #ff964f">
+            <img class="header_images" src="images/header_bg.png">
+        </div>
+    </div>
+    <div class="container" style="background-color: #f02517">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">1</div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">2</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
