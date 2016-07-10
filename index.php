4d6d59cc9ef5296ef22ec68744113865896f9793
gledrej <gledrej1988@gmail.com>
2016-07-10 23:46:38 +0300

    cod

--- index.php
+++ index.php
@@ -57,18 +57,148 @@
 
 
 
-<div class="container-fluid" style="background-color: #0f0f0f">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background-color: #ff964f">
-            <img class="header_images" src="images/header_bg.png">
-        </div>
-    </div>
-    <div class="container" style="background-color: #f02517">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">1</div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">2</div>
-        </div>
-    </div>
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Cod Nasti*/
+
+
 </body>
 </html>
